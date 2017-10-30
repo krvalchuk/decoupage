215518826a4dfcd1b9c6e64efc41696d091fe7d2
krvalchuk <krvalchuk@gmail.com>
2017-10-30 22:41:27 +0200

    main fixes before adaptation for mobile

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var app = angular.module("beautySolutions", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl : "views/main.html"
+            templateUrl: "views/main.html"
         })
         .when("/event", {
-            templateUrl : "views/event.html"
+            templateUrl: "views/event.html"
         });
 });
 
@@ -14,6 +14,19 @@ app.filter("sanitize", ['$sce', function ($sce) {
         return $sce.trustAsHtml(htmlCode);
     }
 }]);
+
+app.filter('range', function () {
+    return function (input, total) {
+        total = parseInt(total);
+
+        for (var i = 0; i < total; i++) {
+            input.push(i);
+        }
+
+        return input;
+    };
+});
+
 app.controller("menuCtrl", function ($scope) {
     $scope.activeMenuId = 'header-link';
 
@@ -25,6 +38,46 @@ app.controller("menuCtrl", function ($scope) {
     }
 });
 
+app.controller("mainEventCtrl", function ($scope) {
+    // function updateMainEventsData(mainEventData, firstEventData, secondEventData) {
+    //    
+    // }
+    //
+    // $.getJSON('./content/main-events/closest-events.json', function (data) {
+    //    
+    //     updateMainEventsData();
+    // });
+});
+app.controller("galleryCtrl", function ($scope) {
+    var view = $("#photosViewer");
+    var move = "72vw";
+    var sliderLimit = -216;
+
+    function pxToVw(value) {
+        return value * (100 / document.documentElement.clientWidth);
+    }
+
+
+    $scope.rightArrowClick = function () {
+        var currentPosition = pxToVw(parseInt(view.css("left")));
+        if (currentPosition >= sliderLimit){
+            view.stop(false, true).animate({left: "-=" + move}, {duration: 400});
+            $("#galleryLeftArrow").removeClass("inactive");
+        } else {
+            $("#galleryRightArrow").addClass("inactive");
+        }
+    };
+
+    $scope.leftArrowClick = function () {
+        var currentPosition = pxToVw(parseInt(view.css("left")));
+        if (currentPosition < 0) {
+            view.stop(false, true).animate({left: "+=" + move}, {duration: 400});
+            $("#galleryRightArrow").removeClass("inactive");
+        } else {
+            $("#galleryLeftArrow").addClass("inactive");
+        }
+    };
+});
 app.controller("eventsCtrl", function ($scope) {
 
     var eventSet1 = [
@@ -59,13 +112,24 @@ app.controller("eventsCtrl", function ($scope) {
     $scope.eventsSets = [eventSet1, eventsSet2];
 });
 app.controller("eventCtrl", function ($scope) {
-    $scope.event={
-        "title":"Роспись ткани <br/> акриловыми красками",
-        "duration":"2-3 часа",
-        "cost":"200",
-        "audience":"для взрослых и детей от 8 лет",
-        "descriptionSet":["Декорируем текстильную эко-сумку или футболку",
+    $scope.event = {
+        "title": "Роспись ткани <br/> акриловыми красками",
+        "duration": "2-3 часа",
+        "cost": "200",
+        "audience": "для взрослых и детей от 8 лет",
+        "descriptionSet": ["Декорируем текстильную эко-сумку или футболку",
             "На мастер-классе мы разберем, как правильно выбирать ткань, рисунок и другие материалы для росписи. Вы научитесь применять одну из техник переноса изображения на ткань и распишите ткань акриловыми красками.",
-            "Приходите и откройте в себе настоящего художника!"]
+            "Приходите и откройте в себе настоящего художника!"],
+        "photosUrl": ["rospis-akrilom"]
+    };
+
+    $scope.updateMainPhoto = function (clickedId) {
+        var curElement = $("#" + clickedId);
+        var mainElement = $("#mainPhoto");
+
+        var mainBg = mainElement.css('background-image');
+
+        mainElement.css('background-image', curElement.css('background-image'));
+        curElement.css('background-image', mainBg);
     }
 });
